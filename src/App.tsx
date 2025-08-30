28644a76ed901d82007620d47cfca4e94bbe72bd
Luke Hewitt <lsjhewitt@gmail.com>
2025-08-30 18:06:12 +1000

    oops we need hash router back

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
